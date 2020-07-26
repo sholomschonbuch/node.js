c3267ae71028a73c5c433da52b2571688fb2ce3b
sholom <you@example.com>
2020-07-26 19:35:56 -0400

    live server

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const path = require("path")
 const hbs = require("hbs")
 const geocode = require("./utils/geocode")
 const forecast = require("./utils/forecast")
+const port = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, "../public")))
 const viewsPath = path.join(__dirname, "../templates/views")
@@ -72,6 +73,6 @@ app.get("*", (req, res) => {
     })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("server up!")
 }) 
